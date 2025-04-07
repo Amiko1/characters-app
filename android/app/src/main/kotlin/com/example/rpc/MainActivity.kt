8b13692704381a52d7d185454de7eec67e2c87af
amiko <amiraniburjanadze@gmail.com>
2025-04-07 18:38:24 +0400

    app init

---
+++ android/app/src/main/kotlin/com/example/rpc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rpc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rpc/screens/home/home.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
