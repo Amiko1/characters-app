4620fa74537961cdf2d3e79e8ea9f1d711fc653f
amiko <amiraniburjanadze@gmail.com>
2025-04-10 18:26:20 +0400

    implement Character model at character card and display it

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -33,3 +33,30 @@ class Character with Stats {
     _skills.add(skill);
   }
 }
+
+List<Character> characters = [
+  Character(
+    id: '1',
+    name: 'Klara',
+    vocation: Vocation.wizard,
+    slogan: 'Kapumf!',
+  ),
+  Character(
+    id: '2',
+    name: 'Jonny',
+    vocation: Vocation.junkie,
+    slogan: 'Light me up...',
+  ),
+  Character(
+    id: '3',
+    name: 'Crimson',
+    vocation: Vocation.raider,
+    slogan: 'Fire in the hole!',
+  ),
+  Character(
+    id: '4',
+    name: 'Shaun',
+    vocation: Vocation.ninja,
+    slogan: 'Alright then gang.',
+  ),
+];
--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:rpc/models/character.dart';
+import 'package:rpc/shared/styled_text.dart';
 import 'package:rpc/theme.dart';
 
 class CharacterCard extends StatelessWidget {
   const CharacterCard(this.character, {super.key});
 
-  final String character;
+  final Character character;
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +15,18 @@ class CharacterCard extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Row(
           children: [
-            Text(character),
-
-            const Expanded(child: SizedBox()),
-
+            Image.asset(
+              "assets/img/vocations/${character.vocation.image}",
+              width: 80,
+            ),
+            SizedBox(width: 20),
+            Column(
+              children: [
+                StyledHeading(character.name),
+                StyledText(character.vocation.title),
+              ],
+            ),
+            Expanded(child: SizedBox()),
             IconButton(
               onPressed: () {
                 // navigate to character profile screen
