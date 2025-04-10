5dab07b2a1740cc0368c3f05652f355cd200a47b
amiko <amiraniburjanadze@gmail.com>
2025-04-10 18:11:33 +0400

    create skills class for skills

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,3 +1,4 @@
+import 'package:rpc/models/skill.dart';
 import 'package:rpc/models/stats.dart';
 import 'package:rpc/models/vocation.dart';
 
@@ -9,16 +10,26 @@ class Character with Stats {
     required this.id,
   });
 
+  // fields
   final Vocation vocation;
   final String name;
   final String id;
   final String slogan;
 
+  final Set<Skill> _skills = {};
   bool _isFav = false;
 
+  // getters
   get isFav => _isFav;
+  get skills => _skills;
 
+  // methods
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkills(Skill skill) {
+    _skills.clear();
+    _skills.add(skill);
   }
 }
