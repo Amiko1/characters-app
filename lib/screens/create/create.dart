b6a9bf7e3cf398fb07c99e2849d3e89463f8e99e
amiko <amiraniburjanadze@gmail.com>
2025-04-13 00:03:25 +0400

    select vocation and submit

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rpc/models/character.dart';
 import 'package:rpc/models/vocation.dart';
 import 'package:rpc/screens/create/vocation_card.dart';
 import 'package:rpc/shared/styled_button.dart';
 import 'package:rpc/shared/styled_text.dart';
 import 'package:rpc/theme.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = const Uuid();
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -17,77 +22,86 @@ class _CreateState extends State<Create> {
   final _nameController = TextEditingController();
   final _sloganController = TextEditingController();
 
-  void handleSubmit() {
-    if (_nameController.text.trim().isEmpty) {
-      print('name must  not be empty');
-
-      return;
-    }
-
-    if (_sloganController.text.trim().isEmpty) {
-      print('slogan must  not be empty');
-
-      return;
-    }
-
-    print(_nameController.text);
-    print(_sloganController.text);
-  }
-
   @override
   void dispose() {
+    // clean up the controller when the widget is disposed
     _nameController.dispose();
     _sloganController.dispose();
-
     super.dispose();
+  }
+
+  // handling vocation selection
+  Vocation selectedVocation = Vocation.junkie;
+
+  void updateVocation(Vocation vocation) {
+    setState(() {
+      selectedVocation = vocation;
+    });
+  }
+
+  // submit handler
+  void handleSubmit() {
+    if (_nameController.text.trim().isEmpty) {
+      // show error in dialog
+      return;
+    }
+    if (_sloganController.text.trim().isEmpty) {
+      // show error in dialog
+      return;
+    }
+
+    characters.add(
+      Character(
+        name: _nameController.text.trim(),
+        slogan: _sloganController.text.trim(),
+        vocation: selectedVocation,
+        id: uuid.v4(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: StyledTitle("Create chracter"), centerTitle: true),
+      appBar: AppBar(title: const StyledTitle('Character Creation')),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
         child: SingleChildScrollView(
           child: Column(
             children: [
+              // welcome message
               Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
-              const Center(child: StyledHeading("Welcome, new player.")),
-              const Center(child: StyledHeading("Create a name & slogan ")),
-
+              const Center(child: StyledHeading('Welcome, new player.')),
+              const Center(
+                child: StyledText('Create a name & slogan for your character.'),
+              ),
               const SizedBox(height: 30),
 
+              // input for name & slogan
               TextField(
                 controller: _nameController,
-                style: GoogleFonts.kanit(
-                  textStyle: Theme.of(context).textTheme.titleMedium,
-                ),
+                cursorColor: AppColors.textColor,
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.person_2),
-                  label: StyledText("Character Name"),
+                  label: StyledText('Character name'),
                 ),
-                cursorColor: AppColors.textColor,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 20),
               TextField(
                 controller: _sloganController,
-                style: GoogleFonts.kanit(
-                  textStyle: Theme.of(context).textTheme.titleMedium,
-                ),
+                cursorColor: AppColors.textColor,
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.chat),
-                  label: StyledText("Character slogan"),
+                  label: StyledText('Character slogan'),
                 ),
-                cursorColor: AppColors.textColor,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
               ),
               const SizedBox(height: 30),
-
-              Center(
-                child: StyledButton(
-                  onPressed: handleSubmit,
-                  child: StyledHeading("Create Character"),
-                ),
-              ),
 
               // select vocation title
               Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
@@ -98,10 +112,40 @@ class _CreateState extends State<Create> {
               const SizedBox(height: 30),
 
               // vocation cards
-              const VocationCard(vocation: Vocation.junkie),
-              const VocationCard(vocation: Vocation.ninja),
-              const VocationCard(vocation: Vocation.wizard),
-              const VocationCard(vocation: Vocation.raider),
+              VocationCard(
+                vocation: Vocation.junkie,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.junkie,
+              ),
+              VocationCard(
+                vocation: Vocation.ninja,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.ninja,
+              ),
+              VocationCard(
+                vocation: Vocation.wizard,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.wizard,
+              ),
+              VocationCard(
+                vocation: Vocation.raider,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.raider,
+              ),
+
+              // good luck message
+              Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+              const Center(child: StyledHeading('Good Luck.')),
+              const Center(child: StyledText('And enjoy the journey ahead...')),
+              const SizedBox(height: 30),
+
+              // submit button
+              Center(
+                child: StyledButton(
+                  onPressed: handleSubmit,
+                  child: const StyledHeading('Create Character'),
+                ),
+              ),
             ],
           ),
         ),
--- lib/screens/create/vocation_card.dart
+++ lib/screens/create/vocation_card.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:rpc/models/vocation.dart';
 import 'package:rpc/shared/styled_text.dart';
+import 'package:rpc/theme.dart';
 
 class VocationCard extends StatelessWidget {
-  const VocationCard({super.key, required this.vocation});
+  const VocationCard({
+    super.key,
+    required this.vocation,
+    required this.onTap,
+    required this.selected,
+  });
 
   final Vocation vocation;
+  final void Function(Vocation) onTap;
+  final bool selected;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            // vocation img
-            Image.asset(
-              'assets/img/vocations/${vocation.image}',
-              width: 80,
-              colorBlendMode: BlendMode.color,
-            ),
-            const SizedBox(width: 10),
-
-            // vocation name & description
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  StyledHeading(vocation.title),
-                  StyledText(vocation.description),
-                ],
+    return GestureDetector(
+      onTap: () {
+        onTap(vocation);
+      },
+      child: Card(
+        color: selected ? AppColors.secondaryColor : Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              // vocation img
+              Image.asset(
+                'assets/img/vocations/${vocation.image}',
+                width: 80,
+                colorBlendMode: BlendMode.color,
+                color:
+                    !selected
+                        ? Colors.black.withValues(alpha: 0.8)
+                        : Colors.transparent,
               ),
-            ),
-          ],
+              const SizedBox(width: 10),
+
+              // vocation name & description
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    StyledHeading(vocation.title),
+                    StyledText(vocation.description),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
